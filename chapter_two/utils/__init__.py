bacd5caf55084252d67960ca9f3d529857cd6c21
hqbcn <hqbcn@outloo.com>
2026-06-06 17:55:23 +0800

    包的使用

---
+++ chapter_two/utils/__init__.py
@@ -0,0 +1,8 @@
+# 描述包信息
+__author__ = "Ethan Huang"
+__version__ = "1.0.0"
+__description__ = "这是一个utils工具包，包含一些常用的函数和类，供其他模块使用。"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2023 Ethan Huang"
+
+__all__ = ["my_fun"] # 定义了__all__变量，指定了当使用from utils import *时，哪些模块会被导入。这里指定了utils模块会被导入，而其他模块不会被导入。
