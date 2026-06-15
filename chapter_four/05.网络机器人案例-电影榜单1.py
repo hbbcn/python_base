87e9b9db01ee29677fe61a8a63a5739ace0203c3
hqbcn <hqbcn@outloo.com>
2026-06-16 01:15:49 +0800

    抓取电影数据并保存到CSV

--- chapter_four/05.网络机器人案例-电影榜单1.py
+++ chapter_four/05.网络机器人案例-电影榜单1.py
@@ -10,10 +10,14 @@ TMDB_BASE_URL = "https://www.themoviedb.org"
 TMDB_TOP_URL = "https://www.themoviedb.org/movie/top-rated"
 
 def save_all_movies(movies):
-    with open(MOVE_LIST_FILE, "w", encoding="utf-8", newline="") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=movies[0].keys())
+    if not movies:
+        print("没有数据可保存")
+        return
+    with open(MOVE_LIST_FILE, "w", encoding="utf-8", newline="") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=movies[0].keys())
         writer.writeheader()
         writer.writerows(movies)
+    print(f"成功保存 {len(movies)} 条电影数据到 {MOVE_LIST_FILE}")
 
 def get_movie_detail(movie_url):
     """
